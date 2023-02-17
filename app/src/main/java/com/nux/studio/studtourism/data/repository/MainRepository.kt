3388af17ab1028924ec2d07f1c37c7057e657c7c
leon-nik <leontev-nikita@yandex.ru>
2023-02-17 21:57:19 +0300

    add_get_universities_repo

---
+++ app/src/main/java/com/nux/studio/studtourism/data/repository/MainRepository.kt
@@ -0,0 +1,35 @@
+package com.nux.studio.studtourism.data.repository
+
+import com.nux.studio.studtourism.data.error.ErrorCatcher
+import com.nux.studio.studtourism.data.error.ErrorRemote
+import com.nux.studio.studtourism.data.remote.RetrofitServices
+import com.nux.studio.studtourism.util.Resource
+import kotlinx.coroutines.flow.flow
+import retrofit2.awaitResponse
+import javax.inject.Inject
+
+class MainRepository @Inject constructor(
+    private val api: RetrofitServices
+) {
+
+    fun getUniversities() = flow<Resource<List<Unit>>> {
+        emit(Resource.Loading(true))
+        val response = try {
+            val responseApi = api.getUniversitiesList().awaitResponse()
+            if (responseApi.code() == 200) {
+                responseApi.body()
+            } else {
+                emit(Resource.Error(ErrorCatcher.catch(responseApi.code())))
+                emit(Resource.Loading(false))
+                return@flow
+            }
+        } catch (e: Exception) {
+            emit(Resource.Error(message = ErrorRemote.NoInternet))
+            emit(Resource.Loading(false))
+            return@flow
+        }
+
+        emit(Resource.Success(response))
+        emit(Resource.Loading(false))
+    }
+}
