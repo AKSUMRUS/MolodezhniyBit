6dcb95d1f82d999d4557b301874cfddb69802f3c
AKSUMRUS <shimorhershow@gmail.com>
2023-02-17 20:17:14 +0300

    feat: add basic all request

--- app/src/main/java/com/nux/studio/studtourism/data/remote/RetrofitServices.kt
+++ app/src/main/java/com/nux/studio/studtourism/data/remote/RetrofitServices.kt
@@ -4,7 +4,9 @@ import com.nux.studio.studtourism.data.remote.models.LoginInfo
 import org.json.JSONObject
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 
 interface RetrofitServices {
 
@@ -12,5 +14,38 @@ interface RetrofitServices {
     fun login(
         @Body info: LoginInfo
     ) : Call<JSONObject>
+
+    @GET("me")
+    fun getProgile()
+
+    @GET("universities/all")
+    fun getUniversitiesList()
+
+    @GET("universities/{id}")
+    fun getUniversity(
+        @Path("id") id: String
+    )
+
+    @GET("dormitories/all")
+    fun getDormitoriesList()
+
+    @GET("dormitories/{id}")
+    fun getDormitory(
+        @Path("id") id: String
+    )
+
+    @GET("events/all")
+    fun getEventsList()
+
+    @GET("reviews")
+    fun getReviews()
+
+    @GET("labs/all")
+    fun getLabsList()
+
+    @GET("labs/{id}")
+    fun getLab(
+        @Path("id") id: String
+    )
 
 }
