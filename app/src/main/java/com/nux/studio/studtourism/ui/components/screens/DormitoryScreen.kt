1f758d824cc27bb066d0dbb96833d01117593f44
i3cheese <i3cheese@yandex.ru>
2023-02-18 17:02:07 +0300

    Dormitory Documents

--- app/src/main/java/com/nux/studio/studtourism/ui/components/screens/DormitoryScreen.kt
+++ app/src/main/java/com/nux/studio/studtourism/ui/components/screens/DormitoryScreen.kt
@@ -1,37 +1,28 @@
 package com.nux.studio.studtourism.ui.components.screens
 
-import androidx.compose.foundation.Image
+import android.content.Intent
+import android.net.Uri
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.vectorResource
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.nux.studio.studtourism.ui.components.atoms.texts.HeadlineH3
-import com.nux.studio.studtourism.ui.components.atoms.texts.HeadlineH4
 import com.nux.studio.studtourism.ui.components.atoms.texts.HeadlineH5
-import com.nux.studio.studtourism.R
 import com.nux.studio.studtourism.data.local.models.*
 import com.nux.studio.studtourism.ui.components.atoms.*
 import com.nux.studio.studtourism.ui.viewmodels.MainViewModel
 import com.nux.studio.studtourism.ui.components.atoms.texts.SectionHeader
-import com.nux.studio.studtourism.ui.theme.Cyan
-import com.nux.studio.studtourism.ui.theme.LightBlue
 
 @Preview
 @Composable
@@ -63,9 +54,11 @@ fun DormitoryScreen(
             HeadlineH3(
                 text = dormitory.details?.mainInfo!!.name, modifier = Modifier.padding(15.dp, 5.dp)
             )
-            Row(modifier = Modifier
-                .padding(10.dp, 0.dp)
-                .background(Color.Transparent)) {
+            Row(
+                modifier = Modifier
+                    .padding(10.dp, 0.dp)
+                    .background(Color.Transparent)
+            ) {
                 dormitory.details.mainInfo.city?.let { city ->
                     Pill(dormitory.details.mainInfo.city, variant = PillVariant.OUTLINE)
                 }
@@ -75,7 +68,7 @@ fun DormitoryScreen(
             if (!minDays.isNullOrEmpty() && !maxDays.isNullOrEmpty()) {
                 Dates("$minDays – $maxDays дней", modifier = Modifier.padding(15.dp, 0.dp))
             }
-            Price(getFormattedPrice(dormitory), modifier = Modifier.padding(15.dp, 0.dp));
+            Price(getFormattedPrice(dormitory), modifier = Modifier.padding(15.dp, 0.dp))
 
             SectionHeader(text = "Адрес", modifier = Modifier.padding(15.dp, 0.dp))
             Text(text = getFormattedAddress(dormitory), modifier = Modifier.padding(15.dp))
@@ -92,6 +85,12 @@ fun DormitoryScreen(
                     Services(services = services)
                 }
             }
+            dormitory.details.documents?.let { documents ->
+                if (documents.isNotEmpty()) {
+                    SectionHeader(text = "Документы", modifier = Modifier.padding(15.dp, 0.dp))
+                    Documents(documents = dormitory.details.documents)
+                }
+            }
         }
     }
 }
@@ -101,10 +100,9 @@ fun Room(
     room: Room,
     modifier: Modifier = Modifier,
 ) {
-    HorizontalCard(color = LightBlue, modifier = Modifier.then(modifier)) {
+    HorizontalCard(color = MaterialTheme.colors.primary, modifier = Modifier.then(modifier)) {
         HeadlineH5(
             text = room.details?.type ?: "Что-то где можно жить",
-//            modifier = Modifier.padding(15.dp)
         )
         Price(formatPrice(room.details?.price), modifier = Modifier)
         room.details?.description?.let { description ->
@@ -118,9 +116,11 @@ fun Rooms(
     rooms: Collection<Room>,
     modifier: Modifier = Modifier,
 ) {
-    HorizontalCardsContainer() {
-        item {
-            rooms.forEach { room -> Room(room = room) }
+    HorizontalCardsContainer(modifier = modifier) {
+        rooms.forEach { room ->
+            item {
+                Room(room = room)
+            }
         }
     }
 }
@@ -130,11 +130,14 @@ fun Service(
     service: Service,
     modifier: Modifier = Modifier,
 ) {
-    HorizontalCard(color = LightBlue, modifier = Modifier.then(modifier)) {
+    HorizontalCard(
+        color = MaterialTheme.colors.primaryVariant,
+        modifier = Modifier.then(modifier)
+    ) {
         HeadlineH5(
             text = service.name ?: "Услуга",
         )
-        Price(formatPrice(service.price));
+        Price(formatPrice(service.price))
         service.description?.let { description ->
             HorizontalCardDescription(text = description)
         }
@@ -150,6 +153,22 @@ fun Services(
         services.forEach { service ->
             item {
                 Service(service)
+            }
+        }
+    }
+}
+
+@Composable
+fun Documents(
+    documents: Collection<DocumentUrl>,
+    modifier: Modifier = Modifier,
+) {
+    val context = LocalContext.current
+    Column(modifier = Modifier.padding(15.dp, 0.dp).then(modifier)) {
+        documents.forEachIndexed { index, documentUrl ->
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(documentUrl))
+            Button(onClick = {context.startActivity(intent)}) {
+                Text(text = "Документ ${index + 1}")
             }
         }
     }
