693ec2eed82934e8e7a892fb8ccc810fc64b3b9c
i3cheese <i3cheese@yandex.ru>
2023-02-18 18:21:06 +0300

    fixes

--- app/src/main/java/com/nux/studio/studtourism/ui/components/screens/DormitoriesScreen.kt
+++ app/src/main/java/com/nux/studio/studtourism/ui/components/screens/DormitoriesScreen.kt
@@ -1,14 +1,10 @@
 package com.nux.studio.studtourism.ui.components.screens
 
 import android.util.Log
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
@@ -18,7 +14,6 @@ import androidx.navigation.NavController
 import com.nux.studio.studtourism.ui.components.molecules.CardDormitory
 import com.nux.studio.studtourism.ui.components.molecules.LoadingViewCenter
 import com.nux.studio.studtourism.ui.viewmodels.MainViewModel
-import com.nux.studio.studtourism.ui.viewmodels.SignUpViewModel
 
 @Composable
 fun DormitoriesScreen(
@@ -30,28 +25,27 @@ fun DormitoriesScreen(
     val mainViewModel: MainViewModel = hiltViewModel()
     var height = (200..400).random()
 
-    if (isLoading) {
     LaunchedEffect(true) {
         viewModel.getDormitories()
     }
 
-    if(isLoading) {
+    if (isLoading) {
         LoadingViewCenter()
     } else {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2),
-        contentPadding = PaddingValues(top = 10.dp, start = 0.dp, end = 0.dp),
-        modifier = Modifier,
-    ) {
-        itemsIndexed(viewModel.state.dormitoriesList) { index, dormitory ->
-                if(index % 2==0){
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(2),
+            contentPadding = PaddingValues(top = 10.dp, start = 0.dp, end = 0.dp),
+            modifier = Modifier,
+        ) {
+            itemsIndexed(viewModel.state.dormitoriesList) { index, dormitory ->
+                if (index % 2 == 0) {
                     height = (200..400).random()
                 }
 
                 CardDormitory(
-                    dormitory = dormitory.details!!,
+                    dormitory = dormitory,
                     onClick = {
-                        Log.e("LOGGGG", viewModel.toString() )
+                        Log.e("LOGGGG", viewModel.toString())
                         navController.navigate(
                             "dormitory?index=$index"
                         )
